2879f9bd7dd5ad4c8fe93158c7fd3cd47a823f89
Admin <Admin@DESKTOP-HJM6B4D>
2021-09-22 11:53:41 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AddressBoook.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBoook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBoook.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
+
+//This interface is an input & output for the database.
+    // public Task<byte[]> is the database input.
+    // public ConvertByteArrayToFile() is the database output
+
+//Task<byte[]> represents an asynchronous operation that returns a byte[] value.
